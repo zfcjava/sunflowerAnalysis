a270411a72a029d9d3668a5a702d63b35bc057b5
zhangfc <18210355391@163.com>
2021-01-25 13:56:47 +0800

    将流程分为3个部分 1.开启协程 2.协程中的耗时操作 3.操作是什么

--- app/src/main/java/com/dirk/sunfloweranalysis/coroutine/async/Coroutines.kt
+++ app/src/main/java/com/dirk/sunfloweranalysis/coroutine/async/Coroutines.kt
@@ -9,10 +9,11 @@ import kotlin.coroutines.suspendCoroutine
 
 fun call(){
     startMyCouroutine {
-        val result = loadImage("123")
-        //接下来使用 result
-
-        //TODO 注意协程不会帮助我们切换线程，需要手动切换线程
+        ioOperation {
+            val result = loadImage("123")
+            //接下来使用 result
+            //TODO 注意协程不会帮助我们切换线程，需要手动切换线程
+        }
     }
 }
 
@@ -21,25 +22,29 @@ fun startMyCouroutine(bolck:suspend()->Unit){
     bolck.startCoroutine(ContextContinuation(AsyncContext()))
 }
 
-suspend fun loadImage(url: String) = suspendCoroutine<ByteArray> {
+
+suspend fun <T>ioOperation(block:()->T) = suspendCoroutine<T> {
     continuation ->
 
 //    val responseBody =
     AsyncTask{
         try {
-            // 假设请求成功
-            if(url!= ""){
-                //因为外层有AsyncTask，切回到主线
-                continuation.resume("ok".encodeToByteArray())
-            } else {
-                continuation.resumeWithException(RuntimeException("http error"))
-            }
+            continuation.resume(block())
         } catch (e:Exception){
             continuation.resumeWithException(e)
         }
     }.execute()
 
 }
+
+fun loadImage(url: String) =
+            // 假设请求成功
+            if(url!= ""){
+                "ok".encodeToByteArray()
+            } else {
+                throw (RuntimeException("http error"))
+            }
+
 
 /**
  * 假设执行了线程切换
